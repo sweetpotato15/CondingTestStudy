9eea3106f2036adfe760b416d608d4c073ad2887
sweetpotato15 <susan6439@naver.com>
2024-07-25 14:44:57 +0900

    등산코스정하기 commit

--- 0731/등산코스정하기.py
+++ 0731/등산코스정하기.py
@@ -2,39 +2,41 @@
 출발지에서 정상까지 가는 길을 최소로 만들면 편도만 구하면 된다. (왕복 고려x)
 -> 힙 자료구조 이용
 
+출발지와 도착지를 지정해서 탐색하기 보다는 모든 출발지를 q에 넣고 제일 먼저 도착하는 지점을 return 할 수 있도록 ()
+-> 여기서 강도가 같다면 도착지 summit 번호가 제일 작은걸 return 해야함
 '''
 import heapq
-def dijkstra(n,start):
-    global distance
-    distance = [0]*(n+1)
-    q = []
-    heapq.heappush(q, (0, start)) # (비용, 지점)
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] >= dist:
-            continue
-        for i in graph[now]:
-            cost = max(cost, distance[i])
-            if cost >= distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q,(cost, i[0]))
-
+from math import inf
 
 def solution(n, paths, gates, summits):
     graph = [[] for _ in range(n+1)] # (도착지, 비용)
-    for path in paths:
-        i,j,w = path
+    for i,j,w in paths:
         graph[i].append([j,w])
         graph[j].append([i,w])
     
-    intensity = int(1e9)
-    top = n+1
-    for i in gates:
-        result = dijkstra(n,i)
-        for j in summits:
-            if distance[j] <= intensity:
-                intensity = distance[i]
-                top = min(top, j)
+    # 모든 출발지 넣기
+    distance = [inf] * (n+1) # 어떤 출발지에서 i까지의 최대 강도
+    q = []
+    for gate in gates:
+        distance[gate] = 0
+        heapq.heappush(q, (0,gate))
+    
+    while q:
+        dist, node = heapq.heappop(q)
+        if distance[node] < dist or node in summits:
+            continue
+            
+        for n_node, value in graph[node]:
+            value = max(value, distance[node])
+            if distance[n_node] > value:
+                distance[n_node] = value
+                heapq.heappush(q, (value, n_node))
                 
-    answer = [intensity, top]
-    return answer
+    result = [-1, inf]
+    for summit in sorted(summits):
+        if distance[summit] < result[1]:
+            result[0] = summit
+            result[1] = distance[summit]
+            
+    return result
+
